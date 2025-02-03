b9d36c0ac52f5787e404883895e12fcee9bed956
thaikana2544 <thaikana2544@gmail.com>
2025-02-03 13:41:29 +0700

    edit code

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 require('dotenv').config();
 const { readdirSync } = require('fs');
-const connectDB = require('./config/db');
 
 const app = express();
 
@@ -12,18 +12,18 @@ const app = express();
 connectDB();
 
 //Middleware
-app.use(morgan('dev'));
 app.use(bodyParser.json({limit: '20mb'}));
-app.use(cors())
+app.use(cors());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.send('Welcom to ArtistMarket!!!')
+    res.send('Welcom to ArtistMarket!')
 });
 
 // Routes
 readdirSync('./routes').map((r)=> app.use(require('./routes/'+r)));
 
-const port = process.env.PORT
+const port = process.env.PORT || 4200;
 app.listen(port,() => {
     console.log('Server is running on port '+port);
 
